736e25787db0b877b70fc4efeca52aa1d4e117eb
global-event-2762 <rosalinschlosser@gmail.com>
2020-04-16 14:35:27 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,7 +66,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.capitalize}:"
     holidays.collect do |holiday, supplies|
       holiday = "#{holiday}".gsub("_", " ")
-      puts "  #{holiday.titlize}: #{supplies.join(", ")}"
+      puts "  #{holiday.capitalize}: #{supplies.join(", ")}"
     end
   end
 end
